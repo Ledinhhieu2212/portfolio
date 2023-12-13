db4957efcd4e0dba2534de12e455d015fec6822e
Ledinhhieu2212 <dinhhieu203765@gmail.com>
2023-12-14 01:16:05 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/portfolio/",o
+  base: "/portfolio/",
   resolve: {
     alias: {
       // Set up the alias for the parent directory using ~
